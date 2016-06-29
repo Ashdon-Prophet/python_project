6af7fe6501574e8c3efb4e2f7e05e0b1aba078d9
Ashdon-Prophet <ashdonprophet@gmail.com>
2016-06-29 13:28:30 -0700

    Updates
    - css/html updates
    - added pictures
    - ready for creation page jquery

--- python_project/apps/main_app/views.py
+++ python_project/apps/main_app/views.py
@@ -6,50 +6,17 @@ from django.core.exceptions import ObjectDoesNotExist
 def index(request):
     return render(request, 'main_app/index.html')
 
-def faq(request):
-    return render(request, 'main_app/faq.html')
-
-def contact(request):
-    return render(request, 'main_app/contact.html')
-
-def four(request):
+def page_not_found(request):
     return render(request, 'main_app/404.html')
 
-def about(request):
-    return render(request, 'main_app/about.html')
-
-def blog1(request):
-    return render(request, 'main_app/blog-home-1.html')
-
-def blog2(request):
+def trade_room(request):
     return render(request, 'main_app/blog-home-2.html')
 
-def blog_post(request):
-    return render(request, 'main_app/blog-post.html')
-
-def portfolio_item(request):
+def profile(request):
     return render(request, 'main_app/portfolio-item.html')
 
-def portfolio1(request):
-    return render(request, 'main_app/portfolio-1-col.html')
+def creator(request):
+    return render(request, 'main_app/test.html')
 
-def portfolio2(request):
-    return render(request, 'main_app/portfolio-2-col.html')
-
-def portfolio3(request):
-    return render(request, 'main_app/portfolio-3-col.html')
-
-def portfolio4(request):
-    return render(request, 'main_app/portfolio-4-col.html')
-
-def pricing(request):
-    return render(request, 'main_app/pricinghtml')
-
-def services(request):
-    return render(request, 'main_app/services.html')
-
-def sidebar(request):
-    return render(request, 'main_app/sidebar.html')
-
-def full_width(request):
-    return render(request, 'main_app/full-width.html')
+def login(request):
+    return render(request, 'main_app/404.html')
